8246694681f785fad02626ea1ec7e07fbf8dc32f
Florian Grazina <Floriangrazina@gmail.com>
2024-11-04 21:22:24 +0100

    Create PlayerController class

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,4 +2,37 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include <EnhancedInputSubsystems.h>
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	// when an entity changes in the server, the clients will be notified
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	// base class BeginPlay
+	Super::BeginPlay();
+
+	// check if the context is valid and setted
+	check(AuraContext);
+
+	// local pointer Subsystem. ULocalPlayer is a static class to access data from the local player
+	// Subsystems are singleton
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+
+	// priority 0
+	Subsystem->AddMappingContext(AuraContext, 0); 
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	// set the input mode to game and UI
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+
+}
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -6,6 +6,9 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+// forward declaration to tell the compiler that the class exists
+class UInputMappingContext;
+
 /**
  * 
  */
@@ -14,4 +17,14 @@ class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	AAuraPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	// property accessible by UE
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
 };
