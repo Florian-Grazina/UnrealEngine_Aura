f8c840601a3ef2a66ae03a74be04eea7075a90ad
Florian Grazina <Floriangrazina@gmail.com>
2024-11-18 21:45:59 +0100

    highlights with post processing

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -2,13 +2,33 @@
 
 
 #include "Character/AuraEnemy.h"
+#include "Aura/Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+}
 
 void AAuraEnemy::HighlightActor()
 {
-	bHighlighted = true;
+	SetHighlight(true);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	bHighlighted = false;
+	SetHighlight(false);
+}
+
+void AAuraEnemy::SetHighlight(bool value)
+{
+	USkeletalMeshComponent* MeshComp = GetMesh();
+
+	int stencilValue = 0;
+	if(value)
+		stencilValue = CUSTOM_DEPTH_RED;
+
+	MeshComp->SetRenderCustomDepth(value);
+	MeshComp->SetCustomDepthStencilValue(stencilValue);
+	Weapon->SetRenderCustomDepth(value);
+	Weapon->SetCustomDepthStencilValue(stencilValue);
 }
